e3f85cf859372ff481d884cf6159da9018c44351
mass-eve <detoxedankur@gmail.com>
2024-02-08 01:13:17 +0530

    Maniplators in c++ : endl and setw() , for now

---
+++ Day02/09-manipulators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is a star pattern" << endl << endl ;
+    cout << setw(9) << "*" << endl;
+    cout << setw(9) << "* *" << endl;
+    cout << setw(9) << "* * *" << endl;
+    cout << setw(9) << "* * * *" << endl;
+    cout << setw(9) << "* * * * *" << endl;
+
+    return 0;
+}
