ca8cbab21ef6099403a2c640dc8e8f462896cf9e
mass-eve <detoxedankur@gmail.com>
2024-02-08 01:10:54 +0530

    how to use the sizeof() function in c++

---
+++ Day02/05-sizeof().cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The size of 34.4 is : " << sizeof(34.4) << endl ;
+    cout << "The size of 34.4l is : " << sizeof(34.4l) << endl ;
+    cout << "The size of 34.4L is : " << sizeof(34.4L) << endl ;
+
+    return 0;
+}
