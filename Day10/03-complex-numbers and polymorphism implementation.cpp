8d8930620fa810d68d96f4054ec4fa97d13718c4
mass-eve <detoxedankur@gmail.com>
2024-02-16 23:10:57 +0530

    added comments

--- Day10/03-complex-numbers and polymorphism implementation.cpp
+++ Day10/03-complex-numbers and polymorphism implementation.cpp
@@ -6,11 +6,17 @@ class ComplexNumber{
         int a, b;
 
     public:
+        // To get the number details (real and imaginary pasrts)
         void getNums();
+
+        // To view the number
         void viewNums();
+
+        // To add two complex numbers and form a new complex number from it
         void joinNums(ComplexNumber one, ComplexNumber two);
+        
+        // To subtract two complex numbers and form a new complex number from it
         void joinNums(ComplexNumber one, ComplexNumber two, int key);
-        // void minusNums(ComplexNumber one, ComplexNumber two, int key = 2);
 };
 
 void ComplexNumber :: getNums(){
@@ -50,13 +56,22 @@ int main()
     ComplexNumber cn1, cn2, cn3, cn4, cn5;
     int key;
 
-    // making cn1
+    // forming complex number 1
     cn1.getNums();
+
+    // forming complex number 2
     cn2.getNums();
+
+    // forming complex number 3 from adding the complex numbers 1 and 2
     cn3.joinNums(cn1, cn2);
+
+    // forming complex number 4 from subtracting complex number 1 and 2
     cn4.joinNums(cn1, cn2, key = 1);
+    
+    // forming complex number 5 from adding the complex numbers 2 and 1
     cn5.joinNums(cn1, cn2, key = 2);
 
+    // Printing all the numbers on the console
     cn1.viewNums();
     cn2.viewNums();
     cn3.viewNums();
